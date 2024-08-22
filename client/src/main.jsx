f004289ee2711f41bfbbdb8e4fe4d964198fac11
Rohan Ramidwar <rohanramidwar71@gmail.com>
2024-08-22 09:34:35 +0530

    redux added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from "./reducers/index.js";
 
-// Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
@@ -14,10 +16,14 @@ if (!PUBLISHABLE_KEY) {
 
 const root = createRoot(document.getElementById("root"));
 
+const store = configureStore({ reducer });
+
 root.render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ClerkProvider>
   </BrowserRouter>
 );
