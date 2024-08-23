b206171f7cc5e77951a5d14051d2eac4c8b70391
Rohan Ramidwar <rohanramidwar71@gmail.com>
2024-08-23 23:59:51 +0530

    fetch auction rooms

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import { getAllRooms } from "@/actions/roomActions";
+import RoomCard from "@/components/RoomCard";
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Homepage = () => {
   const dispatch = useDispatch();
@@ -10,7 +11,18 @@ const Homepage = () => {
     dispatch(getAllRooms());
   }, [dispatch]);
 
-  return <div>Homepage</div>;
+  const { isLoading, rooms } = useSelector((state) => state?.rooms);
+
+  return (
+    <div className="text-slate-800 p-6">
+      <p className="text-2xl">Live auctions</p>
+      <div className="grid grid-cols-6 gap-6 py-6">
+        {rooms.map((room) => (
+          <RoomCard key={room?._id} room={room} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
