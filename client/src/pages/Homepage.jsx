6e3f245b2cb6499b0c58dd58ce9ab735463e57ee
Rohan Ramidwar <rohanramidwar71@gmail.com>
2024-08-30 18:56:48 +0530

    add past auctions cards

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -17,10 +17,13 @@ const Homepage = () => {
     <div className="text-slate-800 p-6">
       <p className="text-2xl">Live auctions</p>
       <div className="grid grid-cols-6 gap-6 py-6">
+        {!rooms.length && "No live auctions found"}
         {rooms.map((room) => (
           <RoomCard key={room?._id} room={room} />
         ))}
       </div>
+      <p className="text-2xl">Past auctions</p>
+      <div className="grid grid-cols-6 gap-6 py-6">No past auctions found</div>
     </div>
   );
 };
