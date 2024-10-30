88ca8e2abcedc0f6be04665d756a2e4596a76bc5
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-10-31 01:34:35 +0530

    Update RoomCard.jsx

--- client/src/components/RoomCard.jsx
+++ client/src/components/RoomCard.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getRoom } from "@/actions/roomActions";
+import moment from "moment";
 
 const RoomCard = ({ room }) => {
   const dispatch = useDispatch();
@@ -22,26 +23,28 @@ const RoomCard = ({ room }) => {
   };
 
   return (
-    <div
-      role="button"
-      onClick={singleRoom}
-      className="p-3 bg-zinc-100 border border-zinc-200 rounded-md hover:shadow-md"
-    >
-      <div className="">
-        <img className="" src={room.itemPic} alt="item-pic" />
+    <div role="button" onClick={singleRoom} className="border border-zinc-300">
+      <div className=" flex justify-center items-center h-56">
+        <img className="p-5 max-h-56" src={room.itemPic} alt="item-pic" />
       </div>
-      <p className="text-xl pt-2">{room?.roomName}</p>
-      <p>
-        Current bid:{" "}
-        <span className="text-lg">
-          &#36;
+      <div className="p-2 pb-5 pt-0">
+        <p className="text-lg pb-5 uppercase text-center">{room?.roomName}</p>
+        <p className="text-sm">
+          {isAuctionEnded ? "Winning Bid" : "Current Bid"}: &#36;
           {room?.currentBid ? room?.currentBid?.bid : 0}{" "}
-        </span>
-      </p>
-      <p>Opening bid: &#36;{room?.openingBid}</p>
-      <p>Ends on: {room?.endsOn}</p>
-      <div className="flex justify-end pt-3">
-        <Button>{isAuctionEnded ? "View bid" : "Place bid"}</Button>
+        </p>
+        <p className="text-sm">Opening Bid: &#36;{room?.openingBid}</p>
+        <p className="text-sm">
+          {isAuctionEnded ? "Ended On" : "Ends On"}:{" "}
+          {moment(room?.endsOn).format("dddd, MMMM Do YYYY, h:mm A")}
+        </p>
+        <div className="flex justify-end pt-3">
+          {isAuctionEnded ? (
+            <Button variant={"link"}>View Bid</Button>
+          ) : (
+            <Button>Place Bid</Button>
+          )}
+        </div>
       </div>
     </div>
   );
