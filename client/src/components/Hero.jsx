136ab99f7b9b88f15e919224bc7c32e38bd6e342
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-10-31 01:31:27 +0530

    Add files via upload

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="text-center bg-gradient-to-r from-red-400 to-violet-400 text-white text-lg p-5 h-40 flex justify-center items-center ">
+      Be in the auction room,
+      <br />
+      wherever you are
+    </div>
+  );
+};
+
+export default Hero;
