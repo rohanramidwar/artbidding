c7f69eef6ec0ea7590e6789a0303c095ad3184dd
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-10-31 01:35:41 +0530

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import { getAllRooms } from "@/actions/roomActions";
+import Hero from "@/components/Hero";
 import RoomCard from "@/components/RoomCard";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,7 +12,7 @@ const Homepage = () => {
     dispatch(getAllRooms());
   }, [dispatch]);
 
-  const { rooms } = useSelector((state) => state?.rooms);
+  const { rooms, isLoading } = useSelector((state) => state?.rooms);
 
   const parseDate = (dateString) => {
     return new Date(dateString.replace(" ", "T"));
@@ -28,21 +29,47 @@ const Homepage = () => {
   );
 
   return (
-    <div className="text-slate-800 p-6 pt-12">
-      <p className="text-2xl">Live auctions</p>
-      <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 py-6">
-        {!liveRooms.length && "No live auctions found 🐣"}
-        {liveRooms.map((room) => (
-          <RoomCard key={room?._id} room={room} />
-        ))}
-      </div>
-      <p className="text-2xl">Past auctions</p>
-      <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 pt-6 pb-12">
-        {!pastRooms.length && "No past auctions found 🐣"}
-        {pastRooms.map((room) => (
-          <RoomCard key={room?._id} room={room} />
-        ))}
-      </div>
+    <div className="text-slate-800">
+      <Hero />
+      <div className="p-5 sm:p-14">
+        <p className="text-xl uppercase">Live auctions</p>
+        {isLoading ? (
+          <div className="flex justify-center items-center py-20">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="py-8">
+            {!liveRooms.length ? (
+              <p>No live auctions found</p>
+            ) : (
+              <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {liveRooms.map((room) => (
+                  <RoomCard key={room?._id} room={room} />
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+
+        <p className="text-xl uppercase">Past auctions</p>
+        {isLoading ? (
+          <div className="flex justify-center items-center py-20">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="py-8">
+            {!pastRooms.length ? (
+              <p>No past auctions found</p>
+            ) : (
+              <div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {pastRooms.map((room) => (
+                  <RoomCard key={room?._id} room={room} />
+                ))}
+              </div>
+            )}
+          </div>
+        )}
+      </div>{" "}
     </div>
   );
 };
