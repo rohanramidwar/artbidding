e8fd0276fe4d73b5f0d49fd93188f7e7681bd1a7
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-10-31 20:24:56 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-center bg-gradient-to-r from-red-400 to-violet-400 text-white text-lg p-5 h-40 flex justify-center items-center ">
+    <div className="text-center bg-gradient-to-r from-pink-500 to-orange-500 text-white text-lg p-5 h-40 flex justify-center items-center ">
       Be in the auction room,
       <br />
       wherever you are
