2faea3c138138315135dd92544667c8e2f70e0ca
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-12-12 23:53:30 +0530

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-center bg-gradient-to-r from-pink-500 to-orange-500 text-white text-lg p-5 h-40 flex justify-center items-center ">
-      Be in the auction room,
-      <br />
-      wherever you are
+    <div className="py-14">
+      <blockquote class="text-2xl font-semibold italic text-center text-slate-900">
+        Be in the{" "}
+        <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+          <span class="relative text-white">auction</span>
+        </span>{" "}
+        room, wherever you are.
+      </blockquote>
     </div>
   );
 };
